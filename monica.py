b284511533db6617a9949c4747d289f8247ae6ca
Ikaro Fernando Sales dos Santos <ikaro.santos@mv.com.br>
2020-02-13 15:09:00 -0300

    Ajuste

--- monica.py
+++ monica.py
@@ -3,17 +3,30 @@ import pandas as pd
 from pandas import read_excel
 
 class Entrega():
-    def setItemEntrega(self, itemEntrega):
+    def __init__(self, itemEntrega, produto, endereco, cep, complemento):
         self.itemEntrega = itemEntrega
-
-    def setProduto(self, produto):
         self.produto = produto
-
-    def setCep(self, cep):
+        self.endereco = endereco
         self.cep = cep
-
-    def setComplemento(self, complemento):
         self.complemento = complemento
+
+def send_response(entrega):
+    url = "https://docs.google.com/forms/d/e/1FAIpQLSfZ0-8mzregRLTmn6xo7q05Camz95F4fkWomU5q8OBSnHWg0g/formResponse"
+    url_test = "https://docs.google.com/forms/d/e/1FAIpQLSfc5gkEAHvXB9zSZYHUewj466yhpE2_hf7r1JzJYu088VgIzA/formResponse"
+    url_monica = "https://docs.google.com/forms/d/e/1FAIpQLSeGkKoum1HEonSxWeZP8r2PDHBncBVxRn61O4x4SwgTILDWtQ/formResponse"
+
+    data = {
+        "entry.11703947"   : "Monica Maria Wanderley Padilha",
+        "entry.283002341"  : entrega.itemEntrega,
+        "entry.96278573"   : entrega.cep,
+        "entry.1587431302" : entrega.endereco,
+        "entry.1495180370" : entrega.complemento
+    }
+
+    print(data)
+    result = requests.post(url_test, data)
+
+    print(result)
 
 def excel_file():
     pd.set_option("display.max_rows", 1000)
@@ -21,29 +34,8 @@ def excel_file():
 
     data = pd.read_excel("automacao.xlsx", "Planilha1")
 
-    for name, sheet in data.items():
-        entrega = Entrega()
-        entrega.setEntrega("aoi")
-
-        print(sheet)
+    # for name, sheet in data.items():
+    entrega = Entrega("Brasil", "Produto A", "Rua Estrela do Mar, 553", "51010-050", "A")
+    send_response(entrega)
 
 excel_file()
-
-def send_response():
-    url = "https://docs.google.com/forms/d/e/1FAIpQLSfZ0-8mzregRLTmn6xo7q05Camz95F4fkWomU5q8OBSnHWg0g/formResponse"
-    url_test = "https://docs.google.com/forms/d/e/1FAIpQLSfc5gkEAHvXB9zSZYHUewj466yhpE2_hf7r1JzJYu088VgIzA/formResponse"
-    url_monica = "https://docs.google.com/forms/d/e/1FAIpQLSeGkKoum1HEonSxWeZP8r2PDHBncBVxRn61O4x4SwgTILDWtQ/formResponse"
-
-    data = {
-        "entry.11703947": "Monica Maria Wanderley Padilha",
-        "entry.283002341": "this",
-        "entry.96278573": "obviously",
-        "entry.1587431302": "works",
-        "entry.1495180370": "8888",
-        "entry.1360790904": "Yes",
-    }
-
-    print(data)
-    result = requests.post(url_test, data)
-
-    print(result)
